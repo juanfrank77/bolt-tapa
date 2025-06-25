ed856c29c60f1191a806c0801f8ef7c5cf4bfd75
Juan F Gonzalez <seminariopv@hotmail.com>
2025-06-24 22:20:27 -0600

    Fix Vite configuration error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { reactRouter } from '@react-router/dev/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    reactRouter(),
     react()
   ],
 });
